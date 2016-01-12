684869a6973c65dd74487bff980394500a84d102
ShimbeyA <shimbeyassie@yahoo.com>
2016-01-12 10:50:20 -0800

    My first version of the site.

---
+++ index2.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <title>Beehive Prototype Home</title>
+  <meta charset="UTF-8">
+  <!--<meta name="author" content="Scarlett Johansson">-->
+</head>
+ <body>
+
+    <form action="record.php" method="post">
+    Hive Name: <input type="text" name="name"><br>
+    Observation date : <input type="text" name="observe"><br>
+    
+
+    Sample Period: <input type="text" name="period"><br>
+    Mite Count : <input type="text" name="mite"><br>
+
+    <input type="submit">
+    </form>
+
+ </body>
+</html>
